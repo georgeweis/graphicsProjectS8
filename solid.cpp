832f91b6e4cb53457f4a98c54b3072d89a79b4c9
Stewart Boogert <stewart.boogert@cockcroft.ac.uk>
2025-02-25 14:05:55 +0000

    solid tidy

--- solid.cpp
+++ solid.cpp
@@ -32,3 +32,11 @@ void solid::print_triangles() const
     }
 }
 
+void solid::print_triangles() const {
+  std::cout << "Printing triangles for the solid:\n";
+  for (size_t i = 0; i < triangles.size(); ++i) {
+    std::cout << "Triangle " << i + 1 << ":\n";
+    triangles[i].print();
+    std::cout << "\n";
+  }
+}
